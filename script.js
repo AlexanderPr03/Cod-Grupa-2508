09a5615715132a51e412166c82c358cbeb229caf
Alexandru <alexander.prohnitchi@gmail.com>
2025-10-05 17:34:46 +0200

    Lectia 12 finisata

---
+++ script.js
@@ -0,0 +1,24 @@
+let prenume = 'Alexandru'
+
+let activitate = 'Mentor Frontend Development'
+
+console.log(prenume)
+console.log(activitate)
+
+let persoana = prenume + ' - ' + activitate;
+console.log(persoana)
+
+let a = 5
+let b = 2
+console.log(a + b)
+
+// DOM - Document Object Model
+
+
+// One line comments 
+
+/*
+
+Multi-line comments
+
+*/
