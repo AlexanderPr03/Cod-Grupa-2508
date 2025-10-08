e62202b9760b202fd54a3450f598fe8d29f29dde
Alexandru <alexander.prohnitchi@gmail.com>
2025-10-08 18:48:39 +0200

    Variabile + tipuri de date

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let prenume = 'Alexandru'
+// let prenume = 'Alexandru'
 
-let activitate = 'Mentor Frontend Development'
+// let activitate = 'Mentor Frontend Development'
 
-console.log(prenume)
-console.log(activitate)
+// console.log(prenume)
+// console.log(activitate)
 
-let persoana = prenume + ' - ' + activitate;
-console.log(persoana)
+// let persoana = prenume + ' - ' + activitate;
+// console.log(persoana)
 
-let a = 5
-let b = 2
-console.log(a + b)
+// let a = 5
+// let b = 2
+// console.log(a + b)
 
 // DOM - Document Object Model
 
@@ -22,3 +22,64 @@ console.log(a + b)
 Multi-line comments
 
 */
+
+
+// TIPURILE DE DATE ELEMENTARE IN JAVASCRIPT
+
+
+// 1) Sirurile de caractere - String
+
+
+let prenume = "Alexandru"
+let nume  = 'Prohnitchi'
+
+// console.log(prenume.toUpperCase()) Fiecare tip de date are anumite functii care pot fi apelate pentru a manipula valorile de acest tip de date intr-un anumit mod (de ex., luam un string si il facem sa fie scris tot cu litere majuscule)
+
+
+// 2) Numerele - Number - si numerele intregi si nr. reale sunt definite de tipul de date Number
+
+// let nr1 = '7'
+// let nr2 = 7
+
+let a = 5
+let b = 7
+
+console.log(prenume + a)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+
+// 3) Boolean - intrerupator, adevarat sau false (true or false)
+
+let passwordMatches = true
+
+// 4) Null - tip de date care reprezinta lipsa unei valori in variabila
+
+let emptyVariable = null;
+
+
+// 5) Undefined - tip de date care reprezinta lipsa variabilei (cel mai des intalnit ca mesaj de eroare)
+
+// undefined
+
+// Cuvantul typeof - cuvant rezervat care ne permite sa vedem tipul de date al unei variabile
+
+console.log(typeof prenume)
+
+
+prenume = 'Ion'
+console.log(prenume)
+
+
+// const - defineste o constanta - o variabila a carei valoare, odata definita, nu mai poate fi modificata.
+// Practica este ca numele consantelor sa fie scris cu majuscule
+const API_KEY = '1231312n22aw9anwa9dnqw9dqwnd912n19d129d1';
+
+API_KEY = 'dada'
+
+
+console.log('test')
+
+
+// OPERATORII IN JAVASCRIPT
