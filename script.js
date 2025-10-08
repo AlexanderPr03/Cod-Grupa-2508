b331bbabe5f902449fbd871a98dba9f7763fe202
Alexandru <alexander.prohnitchi@gmail.com>
2025-10-08 20:00:01 +0200

    Lectia 13 finisata

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ let nume  = 'Prohnitchi'
 let a = 5
 let b = 7
 
-console.log(prenume + a)
+console.log(b + a)
 console.log(a - b)
 console.log(a * b)
 console.log(a / b)
@@ -53,6 +53,10 @@ console.log(a / b)
 // 3) Boolean - intrerupator, adevarat sau false (true or false)
 
 let passwordMatches = true
+
+
+// True - 1
+// False - 0
 
 // 4) Null - tip de date care reprezinta lipsa unei valori in variabila
 
@@ -76,10 +80,120 @@ console.log(prenume)
 // Practica este ca numele consantelor sa fie scris cu majuscule
 const API_KEY = '1231312n22aw9anwa9dnqw9dqwnd912n19d129d1';
 
-API_KEY = 'dada'
+// API_KEY = 'dada'
 
 
 console.log('test')
 
 
 // OPERATORII IN JAVASCRIPT
+
+// 1) Operatorii aritmetici - operatori care primesc 2 valori (numerice sau stringuri sau chiar boolean) si ne intorc inapoi un numar, un string
+
+// +
+// -
+// *
+//  /
+
+
+// ** - ridicarea la putere
+console.log(5 ** 5)
+
+
+// % - impartirea cu rest. Ne da ca si rezultat restul impartirii
+console.log(10 % 3)
+
+// 2) Operatorii de comparatie 
+
+// 2.1) Verificarea egalitatii - Loose equality - Verifica doar valoarea interna, nu si tipul de date
+//   ==
+
+let parolaDeLaUser = 'qwerty'
+let parolaDePeServer = 'qwerty'
+
+console.clear()
+console.log('Parolele corespund: ', parolaDeLaUser == parolaDePeServer);
+
+// 2.2) Verificarea inegalitatii - Loose inequality - Verifica doar valoarea interna, nu si tipul de date
+//  !=
+
+console.log('Parolele nu corespund: ', parolaDeLaUser != parolaDePeServer);
+
+
+// 2.3) Verificarea egalitatii - Strict/Strong Equality - Verifica SI tipul de date SI Valoarea interna
+// ===
+
+
+let idUser1 = '13221312'
+let idUser2 = 13221312
+
+console.log('Utilizatorii corespund: ', idUser1 === idUser2);
+
+// 2.4) Verificarea inegalitatii - Strict/Strong Inequality - Verifica SI tipul de date SI Valoarea interna
+// !==
+
+
+// 2.5, 2.6, 2.7, 2.8) - Comparatile matematice
+// >
+// <
+// >=
+// <=
+
+// Exemplul de mai jos arata ca stringurile pot fi comparate. Una din aplicatiile pentru aceasta functionalitate este ca am putea, de exemplu, sa sortam alfabetic crescator sau descrescator produsele pe o pagina
+console.log('adpwadlpamaazxmqwyqc'  >  'adpwadlpamaazxmqwyqb')
+
+
+
+// 3) Operatorii LOGICI - operatori care ne permit sa combinam mai multe evaluari de adevar intr-o singura conditie complexa
+
+
+// 3.1) Operatorul AND - ȘI -  este folosit atunci când dorim ca toate conditțiile sa fie adevărate pentru ca rezultatul final să fie adevărat
+//  &&
+
+// 1)
+let username = 'Alexand'
+let usernameServer = 'Alexander'
+
+// 2)
+let emailUser = 'alexande@gmail.com'
+let emailServer = 'alexander@gmail.com'
+
+// 3)
+parolaDeLaUser = 'qwerty'
+parolaDePeServer = 'qwerty';
+  
+console.log('Utilizatorul se poate autentifica, toate 3 date sunt corecte: ',
+    (username === usernameServer) && (emailUser === emailServer) && (parolaDeLaUser === parolaDePeServer))
+//                    true                        true                                  false
+// AND - inmultire
+// 1 * 1 * 1
+
+
+// 2) Operatorul OR - SAU - cel puțin o valoarea din cele oferite să fie adevărata
+// ||
+let telefonUser = '0691234'
+let telefonServer = '069123456'
+
+console.log('Cel putin una din autentificari este corecta: ', 
+    (username === usernameServer) || (telefonUser === telefonServer) || (emailUser === emailServer) )
+//            false                            false                                true 
+
+// OR - adunarea
+// 0 + 0 + 0 +  ... + 1 + 1 = 2
+
+// O autentificare completa
+
+console.log('Utilizatorul are toate datele corecte (sau email, telefon, username, si parola corecta):',
+    (username === usernameServer || telefonUser === telefonServer || emailUser === emailServer) 
+            && (parolaDeLaUser === parolaDePeServer))
+
+// (0 + 0 + 0) * 1 = 0
+
+
+// AND are prioritate in ordinea operatiilor (comparatiilor) in fata OR
+
+
+// 3) NOT - negatia
+// !
+
+console.log()
