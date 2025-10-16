42d6bb999fd3dc4dccacc84c3f736cc1e2d00a0b
Alexandru <alexander.prohnitchi@gmail.com>
2025-10-16 19:54:55 +0200

    Lectia 15

--- script.js
+++ script.js
@@ -1,173 +1,17 @@
 
-// OPERATORII IN JAVASCRIPT
-
-// 1) Operatorii aritmetici - operatori care primesc 2 valori (numerice sau stringuri sau chiar boolean) si ne intorc inapoi un numar, un string
-
-// +
-// -
-// *
-//  /
-
-
-// ** - ridicarea la putere
-console.log(5 ** 5)
-
-
-// % - impartirea cu rest. Ne da ca si rezultat restul impartirii
-console.log(10 % 3)
-
-// 2) Operatorii de comparatie 
-
-// 2.1) Verificarea egalitatii - Loose equality - Verifica doar valoarea interna, nu si tipul de date
-//   ==
-
-let parolaDeLaUser = 'qwerty'
-let parolaDePeServer = 'qwerty'
-
-console.clear()
-console.log('Parolele corespund: ', parolaDeLaUser == parolaDePeServer);
-
-// 2.2) Verificarea inegalitatii - Loose inequality - Verifica doar valoarea interna, nu si tipul de date
-//  !=
-
-console.log('Parolele nu corespund: ', parolaDeLaUser != parolaDePeServer);
-
-
-// 2.3) Verificarea egalitatii - Strict/Strong Equality - Verifica SI tipul de date SI Valoarea interna
-// ===
-
-
-let idUser1 = '13221312'
-let idUser2 = 13221312
-
-console.log('Utilizatorii corespund: ', idUser1 === idUser2);
-
-// 2.4) Verificarea inegalitatii - Strict/Strong Inequality - Verifica SI tipul de date SI Valoarea interna
-// !==
-
-
-// 2.5, 2.6, 2.7, 2.8) - Comparatile matematice
-// >
-// <
-// >=
-// <=
-
-// Exemplul de mai jos arata ca stringurile pot fi comparate. Una din aplicatiile pentru aceasta functionalitate este ca am putea, de exemplu, sa sortam alfabetic crescator sau descrescator produsele pe o pagina
-console.log('adpwadlpamaazxmqwyqc'  >  'adpwadlpamaazxmqwyqb')
-
-
-
-// 3) Operatorii LOGICI - operatori care ne permit sa combinam mai multe evaluari de adevar intr-o singura conditie complexa
-
-
-// 3.1) Operatorul AND - ȘI -  este folosit atunci când dorim ca toate conditțiile sa fie adevărate pentru ca rezultatul final să fie adevărat
-//  &&
-
-// 1)
-let username = 'Alexand'
-let usernameServer = 'Alexander'
-
-// 2)
-let emailUser = 'alexande@gmail.com'
-let emailServer = 'alexander@gmail.com'
-
-// 3)
-parolaDeLaUser = 'qwerty'
-parolaDePeServer = 'qwerty';
-  
-console.log('Utilizatorul se poate autentifica, toate 3 date sunt corecte: ',
-    (username === usernameServer) && (emailUser === emailServer) && (parolaDeLaUser === parolaDePeServer))
-//                    true                        true                                  false
-// AND - inmultire
-// 1 * 1 * 1
-
-
-// 2) Operatorul OR - SAU - cel puțin o valoarea din cele oferite să fie adevărata
-// ||
-let telefonUser = '0691234'
-let telefonServer = '069123456'
-
-console.log('Cel putin una din autentificari este corecta: ', 
-    (username === usernameServer) || (telefonUser === telefonServer) || (emailUser === emailServer) )
-//            false                            false                                true 
-
-// OR - adunarea
-// 0 + 0 + 0 +  ... + 1 + 1 = 2
-
-// O autentificare completa
-
-console.log('Utilizatorul are toate datele corecte (sau email, telefon, username, si parola corecta):',
-    (username === usernameServer || telefonUser === telefonServer || emailUser === emailServer) 
-            && (parolaDeLaUser === parolaDePeServer))
-
-// (0 + 0 + 0) * 1 = 0
-
-
-// AND are prioritate in ordinea operatiilor (comparatiilor) in fata OR
-
-
-// 3) NOT - negatia
-// !
-
-console.clear();
-
-
-// 1) Stringuri - au prioritate la conversia de tip. Orice alt tip de date intr-o operatie cu un string va fi convertit de asemenea in string
-
-// 2) Daca operatia nu poate fi facuta cu stringuri (nu este definita, se trece la conversia in numere):
-//  Numere, Booleane
-// La conversia de tip
-// True   - 1
-// False  - 0
-
-// 3) Daca nici operatia cu termenii convertiti in numere nu e posibila (sau termenii nu pot fi convertiti in numere),
-// JS va intoarce inapoi rezultatul NaN - Not a Number
-
-
-
-console.log(5 + '2')
-console.log(5 + 'a')
-console.log(5 - 'a');  // NaN - Not a Number
-
-console.log(true + '2');
-console.log(true - '2');
-console.log(true - 'a');
-console.log(true * 'a');
-console.log(true / '2');
-console.log(true ** 'a');
-
-
-// + - adunarea pentru siruri de caractere = concatenarea - lipirea a doua siruri de caractere
-
-// Null - 0
-// Undefined - NaN
-
-console.log(true + null) // 1
-console.log(true + undefined) // NaN
-
-
-console.clear();
-// CONDITIONALII
-
-// 1) Conditionalul if (daca)
-
-
-parolaDeLaUser = 'qwert'
-parolaDePeServer = 'qwerty'
-
 // if - daca
 // else - in caz contrar
 
-if ((username === usernameServer || telefonUser === telefonServer || emailUser === emailServer) 
-            && (parolaDeLaUser === parolaDePeServer)) {
-    console.log('Utilizatorul este autentificat!');
-    // trimiteti requestul pe server
-    // asteptati raspunsul
-    // procesati raspunsul
-    // etc
-} else {
-    console.log('Parola sau loginul sunt incorecte!')
-}
+// if ((username === usernameServer || telefonUser === telefonServer || emailUser === emailServer) 
+//             && (parolaDeLaUser === parolaDePeServer)) {
+//     console.log('Utilizatorul este autentificat!');
+//     // trimiteti requestul pe server
+//     // asteptati raspunsul
+//     // procesati raspunsul
+//     // etc
+// } else {
+//     console.log('Parola sau loginul sunt incorecte!')
+// }
 
 
 // If-else complet
@@ -270,14 +114,160 @@ for (let i = 0; i<prenume.length; i++) {
 // do {
 //     console.log(f);
 // } while (i<=10)
-let parolaPrompt;
-do {
-    parolaPrompt = prompt('Introdu o parola');
-} while (parolaPrompt != 'qwerty')
+// let parolaPrompt;
+// do {
+//     parolaPrompt = prompt('Introdu o parola');
+// } while (parolaPrompt != 'qwerty')
+
+// TIPURILE DE DATE COMPLEXE
+
+// 1) Obiect
+
+// Definitia unui obiect
+let masina = {
+    // Proprietati ale unui obiect
+    culoare: 'rosu',
+    scaune: 4,
+    cutie_automata: true,
+    motor: {
+        cilindri: 4,
+        volum: 2
+    }
+}
+
+// console.log(masina);
 
 
+// dot notation - notatia punct
+console.log(masina.motor.volum);
 
+// Adaugarea/modificarea unei proprietati
+// masina.motor = '1l';
 
+// Stergerea unei proprietat
+delete masina.culoare;
 
+// console.log(masina)
+// Document Object Model (DOM)
+let articol = {
+    title: 'Cameră de supraveghere IMILAB IMILAB A1 3 MP White',
+    image: 'media/camera.png',
+    pret: 1999,
+    categorii: ['gadgets', 'camere', 'media', 'securitate']
+}
 
+// Ciclul for adaptat pentru obiecte:
+// Ciclul for-in
+for (const proprietate in articol) {
+    console.log(proprietate)
+}
+
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
     
+//     const element = object[key];
+// }
+
+// Consola reprezinta de asemenea un obiect
+console.log(console);
+
+console.clear();
+
+// LISTA (Array, Vector)
+let nume1 = 'Ion'
+let nume2 = 'Maria'
+let nume3 = 'Vlad'
+
+let nume = ['Ion', 'Maria', 'Vlad']
+
+// 
+console.log(nume[3])
+
+// Metode ce pot fi aplicate unei liste
+
+// Adaugam un element la capatul unei liste
+nume.push('Alexandru')
+// Adaugam un element la inceputul unei liste
+nume.unshift('Victor')
+
+
+// Scoatem un element de la capatul listei
+// nume.pop()
+
+
+
+// console.log(nume)
+
+
+// for (let i=0; i<nume.length; i++) {
+//     console.log('Bine ai revenit, ' + nume[i]);
+// }
+
+// Ciclul for-of
+
+// Listele sunt un caz aparte al obiectelor in care numele proprietatilor reprezinta numere consecutive (incepand de la 0)
+console.log(nume);
+
+for (const element of nume) {
+    console.log(element)
+}
+
+// JSON - JavaScript Object Notation
+
+
+let produse = [
+    {
+        name: 'Apple iPhone 13 128 GB',
+        image:'media/iphone.webp',
+        price: 9999
+    },
+    {
+        name: 'Cameră de supraveghere IMILAB IMILAB A1 3 MP White',
+        image:'media/camera.png',
+        price: 1999 
+    },
+    {
+        name: 'Laptop Lenovo IdeaPad Gaming 3 15ACH6-8URK Shadow Black',
+        image:'media/laptop.png',
+        price: 13990 
+    },
+    {
+        name: 'Consolă pentru jocuri Microsoft Xbox Series S White',
+        image:'media/consola.png',
+        price: 5999
+    },
+    {
+        name: 'Boxă portabilă Marshall Emberton Black',
+        image:'media/boxa.png',
+        price: 2999
+    },
+    {
+        name: 'Căști Apple AirPods PRO (2nd generation) MagSafe White',
+        image:'media/casti.png',
+        price: 4999
+    },
+]
+
+
+for (const produs of produse) {
+    console.log(produs);
+}
+
+// Functie - functiile sunt definite de catre noi (utilizator)
+// Metoda - predefinite in JavaScript
+
+// function loginUser() {
+
+
+// }
+
+
+function greetUser(nume) {
+    console.log('Hello ' + nume)
+}
+
+for (const name of nume) {
+    greetUser(name)
+}
+
+// greetUser('Victor')
